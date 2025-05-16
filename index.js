c92fd1fa49e4805a5c1c823a2954a4dc9c7bb7c3
Kevin Lpz <kevin.klv16@gmail.com>
2025-05-16 15:55:36 -0600

    Template
    initial files

---
+++ index.js
@@ -0,0 +1,33 @@
+    const fileInput = document.getElementById('fileInput');
+
+    fileInput.addEventListener('change', function() {
+      const selectedFile = fileInput.files[0];
+      uploadFile(selectedFile);
+    });
+
+
+https://www.google.com/search?q=upload+a+file+js&rlz=1C1GCEA_enMX1158MX1158&oq=upload+a+file+js&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIKCAEQABgTGBYYHjIKCAIQABgTGBYYHjIKCAMQABgTGBYYHjIKCAQQABgTGBYYHjIKCAUQABgTGBYYHjIKCAYQABgTGBYYHjIKCAcQABgTGBYYHjIKCAgQABgTGBYYHjIKCAkQABgTGBYYHtIBCTExNzQyajBqN6gCALACAA&sourceid=chrome&ie=UTF-8
+
+
+
+
+
+
+
+
+
+
+
+/*function Person (first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eye = eye;
+}
+const myFather = new Person("John", "Doe", 50, "blue");
+const myMother = new Person("Sally", "Rally", 48, "green");
+const mySister = new Person("Anna", "Rally", 18, "green");
+
+const mySelf = new Person("Johnny", "Rally", 22, "green");
+
+document.getElementById("demo").innerHTML = mySister.firstName;*/
